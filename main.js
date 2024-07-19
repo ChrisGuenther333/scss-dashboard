a3b5de1a56fad61e49e3bfa378b9ac9cac42ba90
ChrisGuenther333 <CHRIS.GUENTHER4328@STU.MTEC.EDU>
2024-07-19 07:19:50 -0600

    Adding ability to select notification and profile pictures

---
+++ main.js
@@ -0,0 +1,48 @@
+const notif = document.getElementById('notif')
+const notifList = document.getElementById('notifList')
+const pfp = document.getElementById('pfp')
+const pfpList = document.getElementById('pfpList')
+let displayNotif = false
+let displayPfp = false
+
+notif.addEventListener('click', displayNotifList)
+pfp.addEventListener('click', displayPfpList)
+
+function displayNotifList() {
+    let html = ''
+    displayNotif = !displayNotif    
+    if (displayNotif) {
+        displayPfp ? displayPfpList(): displayPfp
+        html =
+        `<ul>
+            <a><li>None</li></a>
+        </ul>`
+    }
+    else {
+        html = ''
+    }
+
+    notifList.innerHTML = html
+
+    console.log(`DisplayNotif: ${displayNotif}`, `DisplayPFP: ${displayPfp}`)
+}
+
+function displayPfpList() {
+    let html = ''
+    displayPfp = !displayPfp
+    
+    if (displayPfp) {
+        displayNotif ? displayNotifList(): displayNotif
+        html =
+        `<ul>
+            <a><li>Profile</li></a>
+            <a><li>Logout</li></a>
+        </ul>`
+    }
+    else {
+        html = ''
+    }
+    pfpList.innerHTML = html
+
+    console.log(`DisplayNotif: ${displayNotif}`, `DisplayPFP: ${displayPfp}`)
+}
